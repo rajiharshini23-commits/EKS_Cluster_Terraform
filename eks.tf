7faac4d6c437d420a988a1a1b4b540bcfe920a4d
raji <rajiharshini23@gmail.com>
2026-09-01 17:23:03 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,43 +1,3 @@
-locals {
-  region        = "us-east-1"
-  name          = "raji_cluster"
-  vpc_cidr      = "10.123.0.0/16"
-  azs           = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
-  }
-}
-
-provider "aws" {
-  region = local.region
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 4.0"
-
-  name = local.name
-  cidr = local.vpc_cidr
-
-  azs             = local.azs
-  private_subnets = local.private_subnets
-  public_subnets  = local.public_subnets
-  intra_subnets   = local.intra_subnets
-
-  enable_nat_gateway = true
-
-  public_subnet_tags = {
-    "kubernetes.io/role/elb" = 1
-  }
-
-  private_subnet_tags = {
-    "kubernetes.io/role/internal-elb" = 1
-  }
-}
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
@@ -58,25 +18,3 @@ module "eks" {
 
   tags = local.tags
 }
-# Addons
-resource "aws_eks_addon" "coredns" {
-  cluster_name      = module.eks.cluster_name
-  addon_name        = "coredns"
-  resolve_conflicts = "OVERWRITE"
-  tags              = local.tags
-}
-
-resource "aws_eks_addon" "kube_proxy" {
-  cluster_name      = module.eks.cluster_name
-  addon_name        = "kube-proxy"
-  resolve_conflicts = "OVERWRITE"
-  tags              = local.tags
-}
-
-resource "aws_eks_addon" "vpc_cni" {
-  cluster_name      = module.eks.cluster_name
-  addon_name        = "vpc-cni"
-  resolve_conflicts = "OVERWRITE"
-  tags              = local.tags
-}
-
