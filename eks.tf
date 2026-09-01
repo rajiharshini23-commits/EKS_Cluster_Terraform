380343a6518000509a0f4de707ae3441df8a498a
raji <rajiharshini23@gmail.com>
2026-09-01 17:17:28 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -43,18 +43,21 @@ module "eks" {
   version = "~> 19.0"
 
   cluster_name    = local.name
- cluster_version = "1.34"
+  cluster_version = "1.34"
   subnet_ids      = module.vpc.private_subnets
   vpc_id          = module.vpc.vpc_id
 
   enable_irsa = true
+
+  # Allow Jenkins to connect to the EKS API
+  cluster_endpoint_private_access = true
+  cluster_endpoint_public_access  = true
 
   # Cluster logging
   cluster_enabled_log_types = ["api", "audit", "authenticator"]
 
   tags = local.tags
 }
-
 # Addons
 resource "aws_eks_addon" "coredns" {
   cluster_name      = module.eks.cluster_name
