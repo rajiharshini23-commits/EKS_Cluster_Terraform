1fbf5c539aa121b11285cc9e69353bcb97fcd3d4
raji <rajiharshini23@gmail.com>
2026-09-01 15:25:24 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -1,48 +1,97 @@
+locals {
+  region        = "us-east-1"
+  name          = "raji_cluster"
+  vpc_cidr      = "10.123.0.0/16"
+  azs           = ["us-east-1a", "us-east-1b"]
+  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
+  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
+  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
+  tags = {
+    Example = local.name
+  }
+}
+
+provider "aws" {
+  region = local.region
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.1"
+  version = "~> 19.0"
 
-  cluster_name                   = local.name
-  cluster_endpoint_public_access = true
+  cluster_name    = local.name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+  enable_irsa = true
 
-  vpc_id                   = module.vpc.vpc_id
-  subnet_ids               = module.vpc.private_subnets
-  control_plane_subnet_ids = module.vpc.intra_subnets
+  # Cluster logging
+  cluster_enabled_log_types = ["api", "audit", "authenticator"]
 
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    ami_type       = "AL2_x86_64"
-    instance_types = ["m5.large"]
-
-    attach_cluster_primary_security_group = true
-  }
-
-  eks_managed_node_groups = {
-    vgs_worker_node = {
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-
-      instance_types = ["t2.medium"]
-      capacity_type  = "SPOT"
-
-      tags = {
-        ExtraTag = "helloworld"
-      }
-    }
+  # Fix IAM inline policy
+  cluster_iam_role_additional_policies = {
+    "raji_cluster-cluster" = jsonencode({
+      Statement = [
+        {
+          Action   = [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        }
+      ]
+      Version = "2012-10-17"
+    })
   }
 
   tags = local.tags
 }
+
+# Addons
+resource "aws_eks_addon" "coredns" {
+  cluster_name      = module.eks.cluster_name
+  addon_name        = "coredns"
+  resolve_conflicts = "OVERWRITE"
+  tags              = local.tags
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name      = module.eks.cluster_name
+  addon_name        = "kube-proxy"
+  resolve_conflicts = "OVERWRITE"
+  tags              = local.tags
+}
+
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name      = module.eks.cluster_name
+  addon_name        = "vpc-cni"
+  resolve_conflicts = "OVERWRITE"
+  tags              = local.tags
+}
+
