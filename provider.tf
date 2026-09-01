d711844c7a1be36d1ffb2303c2e7881e8741df89
raji <rajiharshini23@gmail.com>
2026-09-01 15:36:22 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,31 +7,15 @@ terraform {
   }
 
   # Adding Backend as S3 for Remote State Storage
-backend "s3" {
-  bucket        = "nvdevprojectbucket-2"
-  key           = "eks-cluster/terraform.tfstate"
-  region        = "us-east-1"
-  use_lockfile  = true   # replaces dynamodb_table
-  encrypt       = true
-
+  backend "s3" {
+    bucket       = "nvdevprojectbucket-2"
+    key          = "eks-cluster/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
 
 provider "aws" {
   region = "us-east-1"
 }
-
-locals {
-  region          = "us-east-1"
-  name            = "raji_cluster"
-  vpc_cidr        = "10.123.0.0/16"
-  azs             = ["us-east-1a", "us-east-1b"]
-  public_subnets  = ["10.123.1.0/24", "10.123.2.0/24"]
-  private_subnets = ["10.123.3.0/24", "10.123.4.0/24"]
-  intra_subnets   = ["10.123.5.0/24", "10.123.6.0/24"]
-  tags = {
-    Example = local.name
-  }
-}
-
-
